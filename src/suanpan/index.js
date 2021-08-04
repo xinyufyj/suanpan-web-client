506212014b66bb7672bb6ec0d413d0b2651ff9cf
xinyufyj <feiyujie007@sina.com>
2021-08-04 11:13:43 +0800

    env var

--- src/suanpan/index.js
+++ src/suanpan/index.js
@@ -1,6 +1,5 @@
 import fs from "fs";
 import path from "path";
-import dotEnv from "dotenv";
 import { app } from "electron";
 import { isDevelopment } from "./utils";
 import { SP_SERVER_NAME } from "./constants";
@@ -11,9 +10,6 @@ import logger from "../log";
 
 const AppHome = path.join(app.getAppPath(), '../../');
 const SP_DESKTOP_HOME = path.join(AppHome, '../');
-const dotEnvFile = isDevelopment
-  ? path.join(process.cwd(), "/server/.env")
-  : path.join(AppHome, ".env");
 
 let currentPort = 7000;
 
@@ -30,7 +26,6 @@ export async function launchSuanpanServer() {
   try {
     currentPort = await findFreePort();
     logger.info("find free port:", currentPort);
-    configServerEnv();
     if (isDaemon()) {
       logger.info("daemon mode enabled");
       let isRunning = await isServerRunning();
@@ -59,16 +54,33 @@ export async function launchSuanpanServer() {
         detached: true,
         stdio: "ignore",
         cwd: SP_DESKTOP_HOME,
-        env: {
-          "SP_PORT": `${currentPort}`,
-          "SP_DESKTOP_HOME": SP_DESKTOP_HOME
-        },
+        env: generateEnv(),
       });
     }
-    serverProcess.unref();
+    serverProcess && serverProcess.unref();
+    logger.info(`server spawn success`);
   } catch (e) {
     logger.error(`launch suanpan server failed ${e.message}\n${e.stack}`);
     process.exit(-1);
+  }
+}
+
+function generateEnv() {
+  let defaultCfg = isDevelopment
+      ? "C:\\xuelangyun\\project\\suanpan-web-client\\server\\default.js"
+      : "C:\\snapshot\\suanpan\\config\\default.js";
+    let windowsCfg = isDevelopment
+      ? "C:\\xuelangyun\\project\\suanpan-web-client\\server\\windows.js"
+      : "C:\\snapshot\\suanpan\\config\\windows.js";
+    let localCfg = isDevelopment
+      ? "C:\\xuelangyun\\project\\suanpan-web-client\\server\\local.js"
+      : path.join(SP_DESKTOP_HOME, "config/local.js");
+  return {
+    "SP_PORT": `${currentPort}`,
+    "SP_DESKTOP_HOME": SP_DESKTOP_HOME,
+    "USER_ID": "shanglu",
+    "NODE_ENV": "Windows",
+    "SP_CONFIG": `${defaultCfg},${windowsCfg},${localCfg}`
   }
 }
 
@@ -85,37 +97,3 @@ export async function isServerRunning() {
   return serverProcesses.length > 0;
 }
 
-export function writeDotEnv({ SP_CONFIG, USER_ID, NODE_ENV }) {
-  let content = `
-SP_CONFIG=${SP_CONFIG}
-USER_ID=${USER_ID}
-NODE_ENV=${NODE_ENV}
-`;
-  fs.writeFileSync(dotEnvFile, content);
-}
-
-function configServerEnv() {
-  if (fs.existsSync(dotEnvFile)) {
-    dotEnv.config({ path: dotEnvFile });
-  } else {
-    if (!process.env["SP_CONFIG"]) {
-      let defaultCfg = isDevelopment
-        ? "C:\\xuelangyun\\project\\suanpan-web-client\\server\\default.js"
-        : "C:\\snapshot\\suanpan\\config\\default.js";
-      let windowsCfg = isDevelopment
-        ? "C:\\xuelangyun\\project\\suanpan-web-client\\server\\windows.js"
-        : "C:\\snapshot\\suanpan\\config\\windows.js";
-      let localCfg = isDevelopment
-        ? "C:\\xuelangyun\\project\\suanpan-web-client\\server\\local.js"
-        : path.join(SP_DESKTOP_HOME, "config/local.js");
-      process.env["SP_CONFIG"] = `${defaultCfg},${windowsCfg},${localCfg}`;
-    }
-    if (!process.env["USER_ID"]) {
-      process.env["USER_ID"] = "shanglu";
-    }
-    if (!process.env["NODE_ENV"]) {
-      process.env["NODE_ENV"] = "Windows";
-    }
-    writeDotEnv(process.env);
-  }
-}
